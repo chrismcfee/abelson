3f62789f30e99f7fa3ca22e66b0274e7a2cbe779
Christopher <ggmethos@autistici.org>
2014-08-19 06:19:32 -0400

    abelson.c
    source code for abelson

---
+++ abelson.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(a,b,e,l,s,o,n){
+for(a=-1;;)for(n=pow(1.06,"`cW`g[`cgcg[eYcb^bV^eW^be^bVecb^"[++a&31]+(a&64)/21),b=999;b;
+putchar(128+((8191&l)>b?0:b/8)-((8191&(e+=n))*b-->>16)))l+=a&1?s/2:(s=a&6?s:n/4);}
